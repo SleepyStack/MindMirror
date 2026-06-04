2806112c2fc9bff8574ea1b9090860ee71e6dfaa
Chirag <chirag2006chopra@gmail.com>
2026-06-05 00:56:41 +0800

    add|Auth Service Logic

--- ai_mindmirror/src/main/java/com/thedebugnaths/ai_mindmirror/service/AuthService.java
+++ ai_mindmirror/src/main/java/com/thedebugnaths/ai_mindmirror/service/AuthService.java
@@ -1,5 +1,35 @@
 package com.thedebugnaths.ai_mindmirror.service;
 
+import com.thedebugnaths.ai_mindmirror.dto.AuthRequest;
+import com.thedebugnaths.ai_mindmirror.dto.AuthResponse;
+import com.thedebugnaths.ai_mindmirror.auth.JwtUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class AuthService {
 
+    private final AuthenticationManager authenticationManager;
+    private final UserDetailsService userDetailsService;
+    private final JwtUtil jwtUtil;
+
+    public AuthResponse authenticate(AuthRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.email(),
+                        request.password()
+                )
+        );
+
+        UserDetails userDetails = userDetailsService.loadUserByUsername(request.email());
+
+        String jwtToken = jwtUtil.generateToken(userDetails);
+
+        return new AuthResponse(jwtToken);
+    }
 }
